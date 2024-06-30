70efac1a2e613190e6aa89395eda761c3e10776b
nothing <161198246+PYHOMES00@users.noreply.github.com>
2024-06-30 16:37:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,6 +11,3 @@ API_HASH = os.environ.get("API_HASH", "4022d5686b9b7a7cf8891205921a0ab3")
 
 #Database 
 DB_URI = os.environ.get("DB_URI", "mongodb+srv://madarazbotz:riVNFzoBGQlnkmEA@cluster0.4nx2vpf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-
-#PICS
-PICS = (os.environ.get('PICS', 'https://telegra.ph/file/f69ffb788e27be2d60e84.jpg https://telegra.ph/file/531109a51df28b01d5822.jpg https://telegra.ph/file/4ff2445eb814758548b52.jpg')).split() # Bot Start Picture
