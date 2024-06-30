fe4e5faf9063c8766e9a2e93e156801f41035858
nothing <161198246+PYHOMES00@users.noreply.github.com>
2024-06-30 13:44:54 +0530

    Update save.py

--- Codewithshubham/save.py
+++ Codewithshubham/save.py
@@ -9,7 +9,7 @@ import time
 import os
 import threading
 import json
-from config import API_ID, API_HASH
+from config import API_ID, API_HASH, PICS
 from database.db import database 
 from Codewithshubham.strings import strings, HELP_TXT
 
@@ -70,6 +70,8 @@ async def send_start(client: Client, message: Message):
         InlineKeyboardButton('• ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ •', url='https://t.me/MR_Persis_Bot')
 	]]
     reply_markup = InlineKeyboardMarkup(buttons)
+	await message.reply_photo(
+            photo=random.choice(PICS),
     await client.send_message(message.chat.id, f"<b>👋 Hi {message.from_user.mention}, I am Save Restricted Content Bot, I can send you restricted content by its post link.\n\nFor downloading restricted content /login first.\n\nKnow how to use bot by - /help</b>", reply_markup=reply_markup, reply_to_message_id=message.id)
     return
 
